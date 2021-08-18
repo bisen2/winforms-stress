36f846f89427882bf030fd8727ef987968540f31
Ben Isenhart <bisenhar@uvm.edu>
2021-08-18 18:50:50 -0400

    Refactor TestRunner.fs

--- src/Framework.Stress/TestRunner.fs
+++ src/Framework.Stress/TestRunner.fs
@@ -35,41 +35,37 @@ module TestRunner =
     let resp = MessageBox.Show ("Did this form stutter?", "Stutter Query", MessageBoxButtons.YesNo)
     resp = DialogResult.Yes
 
-  let generateTrace name data =
-    Scatter (
-      x = (data |> List.map (fun (x, _) -> x)),
-      y = (data |> List.map (fun (_, y) -> y)),
-      name = name,
-      mode = "markers",
-      marker = Marker ( size = 10 ) )
-
-  let generatePlot (traces: Trace list) =
-    let layout =
-      Layout (
-        title = "Application stutter as a function of ping rate",
-        xaxis = Xaxis (
-          title = "Ping rate",
-          ``type`` = "log" ),
-        yaxis = Yaxis (
-          title = "Stutter" ) )
-    traces
-    |> Chart.Plot
-    |> Chart.WithLayout layout
-
   let runTests testLength pingRates =
-    let labelUpdateResults =
+
+    let generateTrace name data =
+      Scatter (
+        x = (data |> List.map (fun (x, _) -> x)),
+        y = (data |> List.map (fun (_, y) -> y)),
+        name = name,
+        mode = "markers",
+        marker = Marker ( size = 10 ) )
+
+    let generatePlot (traces: Scatter list) =
+      let layout =
+        Layout (
+          title = "Application stutter as a function of ping rate",
+          xaxis = Xaxis (
+            title = "Ping rate",
+            ``type`` = "log" ),
+          yaxis = Yaxis (
+            title = "Stutter" ) )
+      traces
+      |> Chart.Plot
+      |> Chart.WithLayout layout
+
+    let runTest testName test =
       pingRates
-      |> Seq.map (fun pingRate -> (pingRate, labelUpdateTest testLength pingRate))
+      |> Seq.map (fun pingRates -> (pingRates, test testLength pingRates))
       |> Seq.toList
-    let chartUpdateResults =
-      pingRates
-      |> Seq.map (fun pingRate -> (pingRate, chartUpdateTest testLength pingRate))
-      |> Seq.toList
-    let chartRebuildResults =
-      pingRates
-      |> Seq.map (fun pingRate -> (pingRate, chartRebuildTest testLength pingRate))
-      |> Seq.toList
-    let labelUpdateTrace = generateTrace "Update Label text" labelUpdateResults
-    let chartUpdateTrace = generateTrace "Update Chart" chartUpdateResults
-    let chartRebuildTrace = generateTrace "Rebuild Chart" chartRebuildResults
-    generatePlot [ labelUpdateTrace; chartUpdateTrace; chartRebuildTrace ]
+      |> fun result -> generateTrace testName result
+
+    [ ( "Update label",  labelUpdateTest  );
+      ( "Update chart",  chartUpdateTest  );
+      ( "Rebuild chart", chartRebuildTest ) ]
+    |> List.map (fun (testName, test) -> runTest testName test)
+    |> generatePlot
